ca734924c9c373ae659cfeaf5b443b7ef1f31f63
Michele Tang <tangmichele02@gmail.com>
2023-10-13 19:22:45 -0700

    initial updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 export default function Index() {
+    const myName = "Michele"
+    const myProject = {
+
+    }
+
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+            <div id="container">
+                <div id="top-section">
+                    <h1>Hi, nice to meet you!<br/>I'm {myName}.</h1>
+                </div>
+                <p>
+                    I'm a senior at Pomona College studying <b>Computer Science</b> and <b>Science, Technology, and Society</b>.
+                </p>
+                <p>
+                    You can check out my <a href="https://www.linkedin.com/in/michele-tang/LinkedIn">LinkedIn</a> and <a href="https://github.com/tangmichele02">GitHub</a>. 
+                </p>
+                <p>
+                    Insert
+                </p>
+                <hr/>
+            </div>
         </>
     );
 }
